728d90ce38d81641e195da55888240bcde51a8b0
Kruthika H M <kruthikak338@gmail.com>
2025-11-09 20:00:49 +0530

    Refactor Kadane's algorithm into a function
    Refactored Kadane's algorithm into a function and improved input handling.

--- Kadane's algorithm.py
+++ Kadane's algorithm.py
@@ -1,6 +1,31 @@
 #KADEN's algorithm to find maximum subarray
 """according to this algorithm it gives the contigious list of array elements which gives the maximum sum"""
 
+def kd_algorithm(list_of_numbers):
+    Max=0
+    index=[]
+    check=0
+    if(list_of_numbers[0]<0):
+        return max(list_of_numbers)
+    #actual algorithm begins
+    for inum,value in enumerate(list_of_numbers):
+        if value>(value+check):
+            check=value
+        
+            index.clear()
+            index.append(inum)
+        
+        else:
+            check=value+check
+        if check>Max:
+            index.append(inum)
+            Max=check
+
+    subarray=list_of_numbers[index[0]:index[-1]+1]
+    return Max,subarray
+
+
+
 #this portion deals with inputing a list of numbers
 inputed_list=input("enter the comma seperated numbers only:")
 modified_list=inputed_list.split(",")
@@ -8,29 +33,10 @@ try:
     list_of_numbers = [int(num_str) for num_str in modified_list]
 except ValueError:
     print("invalid input")
-    
-#actual algorithm begins
-max=0
-index=[]
-check=0
-for inum,value in enumerate(list_of_numbers):
-    if value>(value+check):
-        check=value
-        
-        index.clear()
-        index.append(inum)
-        
-    else:
-        check=value+check
-    if check>max:
-        
-        index.append(inum)
-        max=check
 
-subarray=list_of_numbers[index[0]:index[-1]+1]
-print(index)
-print(f"the maximum sum is:{max}")
-print(f"the maximum subarray is:{subarray}")
+result=kd_algorithm(list_of_numbers)
+print(f"the maximum sum and subarray of the given list is: {result}")
+
 
 
 
