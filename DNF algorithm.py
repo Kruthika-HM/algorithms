d4ea21ee310e5b01215dc0bed420a5c53063251e
Kruthika H M <kruthikak338@gmail.com>
2025-11-11 23:17:54 +0530

    Add files via upload
    an algorithm used to sort an array of elements which contains only 0s ,1s and 2s which uses the time O(n) when compared to brute force method O(n^2)

---
+++ DNF algorithm.py
@@ -0,0 +1,18 @@
+def dnf(array):
+    low=0
+    mid=0
+    high=len(array)-1
+    while mid<=high:
+        if array[mid]==0:
+            array[low],array[mid]=array[mid],array[low]
+            low+=1
+            mid+=1
+        elif array [mid]==1:
+            mid+=1
+        else:
+            array[mid],array[high]=array[high],array[mid]
+            high-=1
+    return array
+array=[2,0,1,2,0,1]
+result=dnf(array)
+print(result)
